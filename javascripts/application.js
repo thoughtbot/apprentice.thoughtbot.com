57c3c5ddbd0417b02927aae721e0b1958d71c397
Joshua Clayton <jclayton@thoughtbot.com>
2009-11-02 16:08:01 -0500

    Initial hacks to get project selector working

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,11 +1,33 @@
 $(document).ready(function() {
-  $(".homepage-carousel").jcarousel({
-    initCallback: function(carousel) {
-      $(".homepage-carousel .jcarousel-control a").bind("click", function() {
+  var homepageCarouselCallback = function(selector) {
+    return function(carousel) {
+      $(selector).find(".jcarousel-control a").bind("click", function() {
         $(this).siblings().removeClass("active").end().addClass("active");
         carousel.scroll($.jcarousel.intval($(this).text()));
         return false;
       });
+    };
+  },
+  moveCarousel = function(carousel, direction) {
+    return function() {
+      carousel[direction]();
+      return false;
+    };
+  },
+  carousels = [
+    ".homepage-carousel-umbrella-today",
+    ".homepage-carousel-other"];
+
+  $.each(carousels, function(idx, selector) {
+    $(selector).jcarousel({
+      initCallback: homepageCarouselCallback(selector)
+    });
+  });
+
+  $(".project-carousel").jcarousel({
+    initCallback: function(carousel) {
+      $(".project-carousel .next-project").bind("click", moveCarousel(carousel, "next"));
+      $(".project-carousel .prev-project").bind("click", moveCarousel(carousel, "prev"));
     }
   });
 });
