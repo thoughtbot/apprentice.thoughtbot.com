558300dd3683c61d0dea5db4f33cc841a8560dde
Kevin Burg <kevin@swift.local>
2009-11-09 09:01:08 -0500

    Added remaining project images.

--- javascripts/application.js
+++ javascripts/application.js
@@ -26,7 +26,7 @@ $(document).ready(function() {
     }
   });
 
-  $.each([".homepage-carousel-shortbord", ".homepage-carousel-umbrella-today", ".homepage-carousel-carbonrally", ".homepage-carousel-buywithme"], function(idx, selector) {
+  $.each([".homepage-carousel-shortbord", ".homepage-carousel-umbrella-today", ".homepage-carousel-carbonrally", ".homepage-carousel-buywithme",  ".homepage-carousel-naturenetwork",  ".homepage-carousel-teal"], function(idx, selector) {
     $(selector + " .slides").cycle({
       fx: "fade",
       timeout: 0,
