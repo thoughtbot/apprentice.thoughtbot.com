b650d27273119ebb7643f17335257c7b64d1c435
Fred Yates <fred@fred-yatess-macbook-pro.local>
2010-04-16 16:11:05 -0400

    fixed homepage slider with chad's help

--- javascripts/application.js
+++ javascripts/application.js
@@ -27,7 +27,7 @@ $(document).ready(function() {
     }
   });
 
-  $.each([".homepage-carousel-shortbord", ".homepage-carousel-umbrella-today", ".homepage-carousel-carbonrally", ".homepage-carousel-buywithme", ".homepage-carousel-teal"], function(idx, selector) {
+  $.each([".homepage-carousel-pozit", ".homepage-carousel-safetystream", ".homepage-carousel-shortbord", ".homepage-carousel-gemcutter"], function(idx, selector) {
     $(selector + " .slides").cycle({
       fx: "fade",
       timeout: 0,
