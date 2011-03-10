eb46afbc4d554bc36af2ed0d1ebba8fcf07f926b
Kyle Fiedler <kfiedler@thoughtbot.com>
2011-03-10 11:40:18 -0500

    Added copycopter and gulf coast to the portfolio

---
+++ s/config/initializers/session_store.rb
@@ -0,0 +1,15 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying cookie session data integrity.
+# If you change this key, all old sessions will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.session = {
+  :key         => '_s_session',
+  :secret      => 'fa5d2909693303438e5620dfb35e227ab1827ca005096fb6f2799fb4379226017223dfdacb1f0f4dd4f0e33606a56ba03171b1f61231d5dd509fc2d816645f24'
+}
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# ActionController::Base.session_store = :active_record_store
