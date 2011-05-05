fb81d1fe9cf23b56ec944d668e3b8c73ac2604ee
Kyle Fiedler <kfiedler@thoughtbot.com>
2011-05-05 09:56:58 -0400

    Edited a bunch of the secondary pages

--- javascripts/application.js
+++ javascripts/application.js
@@ -3,6 +3,11 @@ $.jqwidont.auto(false);       // disable it
 
 $(document).ready(function() {
   $('p').widont();
+  
+  //slider stuff
+  $('#gulfcoast-slider').hide();
+  
+  $('#gulfcoast a').click(function(){
+    $('#gulfcoast-project').slideToggle();
+  });
 });
-
-
