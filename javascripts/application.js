1e1323cc73ceb385c34572d7ef44a39526bcbbc3
Kyle Fiedler <kfiedler@thoughtbot.com>
2011-05-05 18:36:24 -0400

    CLeaned up the design portfolio and started to get the main portfolio working

--- javascripts/application.js
+++ javascripts/application.js
@@ -3,11 +3,27 @@ $.jqwidont.auto(false);       // disable it
 
 $(document).ready(function() {
   $('p').widont();
-  
-  //slider stuff
-  $('#gulfcoast-slider').hide();
-  
-  $('#gulfcoast a').click(function(){
-    $('#gulfcoast-project').slideToggle();
+
+  // slider stuff
+  $('#portfolio-slider').hide();
+
+  $('#portfolio a').click(function(){
+    var divname = this.title
+
+    if ($('#portfolio-slider:hidden').length) {
+      $('#portfolio-slider').show();
+      $("#"+divname+"-project").slideDown();
+      return false;
+    } else {
+      return false;
+    }
   });
+});
+
+$(window).load(function() {
+    $('#portfolio-slider div ul.slider').nivoSlider({
+      effect:'fade',
+      directionNavHide:false,
+      controlNav:false
+    });
 });
