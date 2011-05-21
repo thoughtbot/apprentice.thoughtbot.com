1d59876c70b212b426d3daea21d33de77dd4500b
Nick Quaranto <nick@quaran.to>
2011-05-20 23:02:46 -0400

    dynamically load the portfolio images

--- javascripts/application.js
+++ javascripts/application.js
@@ -4,29 +4,47 @@ $.jqwidont.auto(false);       // disable it
 $(document).ready(function() {
   $('p').widont();
 
-  // slider stuff
   $('#portfolio-slider, #portfolio-slider div').hide();
-
   $('#portfolio').localScroll();
 
+  var images = {
+    "gulfcoast": 4,
+    "pozit": 3,
+    "coupondudes": 4,
+    "gnite": 2,
+    "umbrellatoday": 2,
+    "carbonrally": 2,
+    "saftystream": 4,
+    "shortboard": 4
+  };
+
   $('#portfolio a').click(function(){
-    var divname = this.title
+    var divname = this.title;
+    var slider  = $("#" + divname + "-project")
 
-
-    if ($('#portfolio-slider:hidden').length) {
+    if ($('#portfolio-slider:hidden').size() == 1) {
       $('#portfolio-slider').slideDown();
-      $("#"+divname+"-project").slideDown();
-      return false;
+      slider.slideDown();
     } else {
-      $("#"+divname+"-project").slideDown().siblings().slideUp();
-      return false;
-    };
-  });
+      slider.slideDown().siblings().slideUp();
+    }
 
-  $('#portfolio-slider div ul.slider').nivoSlider({
-    effect:'sliceDown',
-    manualAdvance:true,
-    directionNavHide:false,
-    controlNav:false
+    if (slider.find("li").size() == 1) {
+      var list = slider.find("ul");
+      list.empty();
+
+      for(var i = 1; i <= images[divname]; i++) {
+        list.append("<li><img src='/images/portfolio/large/" + divname + "-" + i + ".png' /></li>");
+      }
+
+      list.nivoSlider({
+        effect:'sliceDown',
+        manualAdvance:true,
+        directionNavHide:false,
+        controlNav:false
+      });
+    }
+
+    return false;
   });
 });
