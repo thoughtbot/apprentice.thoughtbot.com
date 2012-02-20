9e8ed931e21335417d13680360778be66e7085d8
adarshpandit <adarshpandit@gmail.com>
2012-02-20 15:14:33 -0500

    Added Adarsh Pandit

--- assets/giantrobots/javascripts/authors.js
+++ assets/giantrobots/javascripts/authors.js
@@ -1,6 +1,7 @@
 jQuery(document).ready(function() {
   authors = {
     asimeoni: 'Angelo Simeoni',
+    apandit: 'Adarsh Pandit',
     codeulate: 'Ben Orenstein',
     cpytel: 'Chad Pytel',
     dancroak: 'Dan Croak',
