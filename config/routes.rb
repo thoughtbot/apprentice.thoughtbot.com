0166949c050578e35ce0b7b95078190edfa9dc71
Gabe Berke-Williams <gabe@thoughtbot.com>
2012-11-01 20:39:26 -0400

    Revel in the majesty of bourbon.io

--- config/routes.rb
+++ config/routes.rb
@@ -12,10 +12,10 @@ ThoughtbotDotCom::Application.routes.draw do
   get 'training' => redirect('http://learn.thoughtbot.com')
   get 'd4d-resources' => redirect('http://learn.thoughtbot.com/d4d-resources')
 
-  # Temporary Bourbon and Neat redirects
-  get 'bourbon' => redirect('http://thoughtbot.github.com/bourbon')
-  get 'neat' => redirect('http://thoughtbot.github.com/neat')
-  get 'neat/examples' => redirect('http://thoughtbot.github.com/neat/examples')
+  # Bourbon and Neat redirects
+  get 'bourbon' => redirect('http://bourbon.io/')
+  get 'neat' => redirect('http://neat.bourbon.io/')
+  get 'neat/examples' => redirect('http://neat.bourbon.io/examples/')
 
   get 'projects/cocaine' => redirect('http://github.com/thoughtbot/cocaine')
   %w(clearance factory_girl jester mile_marker pacecar paperclip shoulda squirrel).each do |project|
