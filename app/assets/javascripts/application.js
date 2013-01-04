9ca458eaf10ff4a5cb268d877a3863e34c35164a
Edwin <edwin.h.morris@gmail.com>
2013-01-04 17:30:01 -0500

    add sticky scrolling to case study text * use jquery stickem * add listeners for scroll direction * fix stickit on mobile and short content

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery-1.4.3.min
+//= require jquery-1.8.3.min
 //= require_tree .
 
 // disable automatic initialization
@@ -18,8 +18,6 @@ $.jqwidont.auto(false);
 
 $(document).ready(function() {
   $('p').widont();
-
-  $('#portfolio').localScroll();
 
   $(".event-coupon-wrapper strong:contains('Webcast')").addClass("webcast");
   $(".event-coupon-wrapper strong:contains('Workshop')").addClass("workshop");
